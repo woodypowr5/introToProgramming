8e79fa40c635be5a6a9cd3d8e122d841ce3ff2b9
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-04-09 19:53:22 -0700

    first commit

---
+++ chapter1_3/chapter1_4/chapter1_4/chapter1_4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chapter1_4
+//
+//  Created by Christopher Woodward on 4/9/18.
+//  Copyright © 2018 Christopher Woodward. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
