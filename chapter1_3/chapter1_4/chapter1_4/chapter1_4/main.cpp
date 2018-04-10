67880e1d5e61b88a58252809ff4fe3046a27d498
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-04-09 19:57:38 -0700

    next prob

--- chapter1_3/chapter1_4/chapter1_4/chapter1_4/main.cpp
+++ chapter1_3/chapter1_4/chapter1_4/chapter1_4/main.cpp
@@ -1,15 +1,27 @@
-//
-//  main.cpp
-//  chapter1_4
-//
-//  Created by Christopher Woodward on 4/9/18.
-//  Copyright © 2018 Christopher Woodward. All rights reserved.
-//
+// Write a prgoram that prompts the user to enter the
+// distance to drive, the fuel efficiency of the car
+// in miles per gallon, and then displays the cost of the trip
 
 #include <iostream>
+#include <math.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+
+    int distance;
+    int fuelEfficiency;
+    int pricePerGallon;
+    int cost;
+    
+    cout << "Enter the distance of the trip" << endl;
+    cin >> distance;
+    cout << "Enter the price per gallon: " << endl;
+    cin >> pricePerGallon;
+    cout << "Enter the fuel efficiency: " << endl;
+    cin >> fuelEfficiency;
+    
+    cost = distance / fuelEfficiency * pricePerGallon;
+    
+    cout << "The cost for the trip is: ";
+    cout << cost << endl;
 }
